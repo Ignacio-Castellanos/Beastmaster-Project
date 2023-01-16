f84a00f3fa2405103805a9efad95599a11977b4e
lucia <luciatiradoturiel@gmail.com>
2023-01-16 09:23:09 +0100

    shop

---
+++ data.js
@@ -0,0 +1,45 @@
+let listaDiscos = [
+    {
+        nombreArtista: "Amy Winehouse",
+        tituloDisco: "Back to black",
+        añoLanzamiento: "2006",
+        precio: "20€",
+        image: scr = "images/Amy Winehouse.jpg" 
+        },
+    {
+        nombreArtista: "Joaquin Sabina",
+        tituloDisco: "19 días y 500 noches",
+        añoLanzamiento: "1999",
+        precio: "15€",
+        image: scr = "images/Joaquin Sabina.jpg",
+        },
+    {
+        nombreArtista: "Kiss",
+        tituloDisco: "Dynasty",
+        añoLanzamiento: "1979",
+        precio: "17€",
+        image: scr = "images/dynasty-kiss.jpg",
+        },
+    {
+        nombreArtista: "Oasis",
+        tituloDisco: "Wanderwall",
+        añoLanzamiento: "1995",
+        precio: "18€",
+        image: scr = "images/oasis.jpg",
+        },
+    {
+        nombreArtista: "Queen",
+        tituloDisco: "Greatest Hits",
+        añoLanzamiento: "1981",
+        precio: "20€",
+        image: scr = "images/queen.jpg",
+        },           
+    {
+        nombreArtista: "ACDC",
+        tituloDisco: "For those about to rock",
+        añoLanzamiento: "1981",
+        precio: "20€",
+        image: scr = "images/acdc.jpg",
+        },
+    ];
+
---
+++ storage.js
@@ -0,0 +1,25 @@
+const saveItem = () => {
+    let item = document.getElementById("item").value;    
+    localStorage.setItem("disco", item);
+    console.info("Item creado en el localStorage");
+}
+
+const getItem = () => {
+    let itemDisco = localStorage.getItem("disco");
+    if(itemDisco) {
+        console.info(`Item traido del localStorage: ${itemDisco}`);        
+    } else {
+        console.info("No hay item en localStorage");
+    }
+}
+
+const deleteItem = () => {
+    localStorage.removeItem("disco");
+    console.info("Has borrado un item del localStorage");
+}
+
+const deleteAll = () => {
+    localStorage.clear();
+    console.info("Has borrado todos los items del localStorage");
+}
+
